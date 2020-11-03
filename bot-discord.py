8e917144d018beac60dbb7c2d190a164ff2dcb3a
ildyria <beviguier@gmail.com>
2020-11-03 11:28:47 +0100

    add R6 and more flexibility

--- bot-discord.py
+++ bot-discord.py
@@ -4,13 +4,18 @@ import discord
 import asyncio
 import os.path
 import random
+import re
+
+result = re.match(pattern, string)
+
 
 async def print_sad(message):
     await message.channel.send(':disappointed_relieved:')
     return 0
 
 def rdm_blob():
-    blobs = [':blobcool:', ':blobaww:',':blobfistbumpR:',':blobmoustache:',':blobsmileopenmouth:',':blobpoliceangry:',':blobhammer:',':fire_engine:', ':googlefire']
+    # blobs = [':blobcool:', ':blobaww:',':blobfistbumpR:',':blobmoustache:',':blobsmileopenmouth:',':blobpoliceangry:',':blobhammer:',':fire_engine:', ':googlefire:']
+    blobs = [':fire_engine:', ':googlefire:', ':fire_extinguisher:', ':camera:', ':detective:', ':rage:']
     return random.choice(blobs)
 
 
@@ -29,35 +34,25 @@ class MyClient(discord.Client):
         print(self.user.id)
         print('---------------------------------')
 
-    def checkR5(self,message):
-        return (message.content.find("my r5") >= 0) or (message.content.find("my R5") >= 0)
-
-    async def ourR5(self,message):
-        await message.channel.send(message.author.mention + ", it's **OUR R5**! " + rdm_blob())
+    async def my(self, message, obj, printed):
+        p = re.compile('my\s' + obj, re.IGNORECASE)
+        if p.match(message):
+            await message.channel.send(message.author.mention + ", it's **OUR " + printed + "**! " + rdm_blob())
 
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
 
-        if self.checkR5(message):
-            await self.ourR5(message)
+        await self.my(message, 'R5', 'R5')
+        await self.my(message, 'R6', 'R6')
+        await self.my(message, '70-135', '70-135mm')
 
 if __name__ == "__main__":
     with open('.key', 'r', encoding="utf-8") as file:
         cred_data = file.read().split('\n')
-        # print(cred_data[0])
         client = MyClient()
         client.run(cred_data[0])
-
-# print(message.author)
-# print(message.author.mention)
-# print(message.author.id)
-# print(self.config.users)
-
-# id_user = st[2]
-# user = discord.utils.get(message.channel.members, name = 'ildyria', discriminator = 7128)
-# await message.channel.send(id_user)
 
 # give users a link to invite this bot to their server embed.add_field(name="Invite", value="[Invite link](
 # https://discord.com/api/oauth2/authorize?client_id=772901093645484086&permissions=126016&scope=bot)")
